ee04c02087f4d023aa8fa014a96ae895113c2c2b
Documenter.jl <documenter@juliadocs.github.io>
2022-05-24 21:29:53 +0000

    build based on 7416280

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScalarWave","category":"page"},{"location":"#ScalarWave","page":"Home","title":"ScalarWave","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScalarWave.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScalarWave]","category":"page"}]
+}
